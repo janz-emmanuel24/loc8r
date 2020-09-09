26e4c28c8246a6274b7adcdd93d55a3f3c8a2669
janz-emmanuel24 <jansabbas1@gmail.com>
2020-09-09 17:57:02 +0300

    models and dbConnect

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,7 +1,8 @@
 /* GET homepage */
 const about = (req, res) => {
     res.render('generic-text', {
-        title: 'About'
+        title: 'About Loc8r',
+        content: 'Loc8r was created to help people find places to sit down and get a bit of work done. \n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan'
     });
 };
 
---
+++ app_server/models/db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+let dbURL = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+    dbURL = `${process.env.MONGODB_URL}`;
+}
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURL}`);
+});
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:!!', err);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected!');
+});
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+    });
+    callback();
+};
+//  For nodemon restarts
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
+
+/* Bringing locationModel into the application */
+require('./locations');
